6fdc85e3671e43afc82106a78442f7676fd9e3f7
Paribartan Dhakal <paribartandhakal@pari.local>
2019-11-17 22:37:07 -0600

    print LCS

--- DynamicProgramming/LCS.cpp
+++ DynamicProgramming/LCS.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <algorithm>
 
-
 using namespace std;
 
 template<typename dataType>
@@ -10,7 +9,7 @@ int LCSRecursion(dataType X, dataType Y, int m, int n);
 
 
 template<typename dataType>
-int LCSMemoization(dataType X, dataType Y, int m, int n);
+void LCSMemoizationAndPrint(dataType X, dataType Y, int m, int n);
 
 
 int main() {
@@ -18,7 +17,8 @@ int main() {
     string X = "ABXBDAB", Y = "BDCABA";
 
     cout<<"The length of the longest common subsequence of strings using Recursion = "<<LCSRecursion<string>(X, Y, X.length(), Y.length())<<endl;
-    cout<<"The length of the longest common subsequence of strings using Memoization = "<<LCSMemoization<string>(X, Y, X.length(), Y.length())<<endl;
+
+    LCSMemoizationAndPrint<string>(X, Y, X.length(), Y.length());
     return 0;
 }
 
@@ -41,32 +41,57 @@ int LCSRecursion(dataType X, dataType Y, int m, int n) {
 
 
 template<typename dataType>
-int LCSMemoization(dataType X, dataType Y, int m, int n) {
+void LCSMemoizationAndPrint(dataType X, dataType Y, int m, int n) {
 
-    int c[m+1][n+1];
+    int lookupTable[m+1][n+1];
 
     //lookUp table stores solutions to already solved sub problems
 
     //first column and row of lookUp table is 0
     for (int i = 0; i < m+1; i++) {
-        c[i][0] = 0;
+        lookupTable[i][0] = 0;
     }
 
     for (int j = 0; j < n+1; j++) {
-        c[0][j] = 0;
+        lookupTable[0][j] = 0;
     }
 
     for (int i = 1; i < m+1; i++) {
         for(int j = 1; j < n+1; j++) {
             if(X[i-1] == Y[j-1]) {
-                c[i][j] = c[i-1][j-1] + 1;
+                lookupTable[i][j] = lookupTable[i-1][j-1] + 1;
             }
             else {
-                c[i][j] = max(c[i-1][j], c[i][j-1]);
+                lookupTable[i][j] = max(lookupTable[i-1][j], lookupTable[i][j-1]);
             }
         }
     }
 
     //LCS will be the last entry in the lookUp table
-    return c[m][n];
+    cout<<"The length of the longest common subsequence of strings using Memoization = "<<lookupTable[m][n]<<endl;
+    int length = lookupTable[m][n];
+
+    char lcs[length+1]; //character array to store the lcs string
+    lcs[length] = '\0';
+
+    int i = m, j = n;
+
+    while (i > 0 && j > 0){
+
+        if(X[i-1] == Y[j-1]) {
+            lcs[length-1] = X[i-1];
+            i--, j--, length--; 
+        }
+        else if (lookupTable[i-1][j] > lookupTable[i][j-1])
+            i--;
+        else
+            j--;
+    }
+
+    cout << "One of the LCS of " << X << " and " << Y << " is " << lcs << endl; 
+
+
+    
+    
 }
+
